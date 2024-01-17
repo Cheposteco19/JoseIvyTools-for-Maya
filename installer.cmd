0d8c0aa667b25a00789883424dad3dc8dace2b4c
Jose <jose123ma@hotmail.com>
2024-01-17 17:45:19 -0500

    Update to 2024

--- installer.cmd
+++ installer.cmd
@@ -1,4 +1,4 @@
 @echo off
 xcopy /s/y "%cd%\baking_tools" "%userprofile%\Documents\maya\scripts\baking_tools\"
 xcopy /s/y "%cd%\uv_tools" "%userprofile%\Documents\maya\scripts\uv_tools\"
-xcopy /s/y "%cd%\shelf_Jose_Tools.mel" "%userprofile%\Documents\maya\2023\prefs\shelves\"
+xcopy /s/y "%cd%\shelf_Jose_Tools.mel" "%userprofile%\Documents\maya\2024\prefs\shelves\"
