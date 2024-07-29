5c825dfa25398eaff52b4a3b0673fbbbf01826a3
ARTURO\jose1 <jose123ma@hotmail.com>
2024-07-29 13:14:27 -0400

    Iteration 2 Jose Ivy

--- baking_tools/ui.py
+++ baking_tools/ui.py
@@ -1,22 +1,27 @@
 from baking_tools import core as baking_tools_core
 from maya import cmds
 import maya.mel as mm
-import json
+import pickle
 import os
 
+WORKSPACE_CONTROL_STATE_NAME='{}_workspace_control_state'.format(os.path.basename(cmds.file(query=True, sceneName=True)) or "untitled")
+WORKSPACE_CONTROL_STATE_ROOT_DIR = '{}/{}/prefs'.format(mm.eval('getenv "MAYA_APP_DIR";'), cmds.about(version=True))
+WORKSPACE_CONTROL_STATE_EXT= 'pck'
+
+WORKSPACE_CONTROL_STATE_PATH='{}/{}.{}'.format(WORKSPACE_CONTROL_STATE_ROOT_DIR,WORKSPACE_CONTROL_STATE_NAME,WORKSPACE_CONTROL_STATE_EXT)
+
+
+
 #Window components names
-BAKE_WINDOW_NAME= 'bake_test_ui'
+DISPLAY_LAYER_WORKSPACE_CONTROL_NAME= 'displayLayerWorkspaceControl'
 AUTOUNWRAP_CHECK_BOX_NAME= 'auto_unwrap_check_box'
-PATH_TEXT_BOX_NAME='path_text_box'
 BROWSE_BUTTON_NAME_DICT={}
-DOCK_CONTROL_NAME='bake_tester_dock_control'
-LAYER_LIST_NAME='layer_list'
 
 #Save preferances settings
 
-DIRECTORY_HISTORY_NAME='directory_history'
-DIRECTORY_HISTORY_ROOT_DIR = mm.eval('getenv "MAYA_APP_DIR";')
-DIRECTORY_HISTORY_EXT= 'json'
+DIRECTORY_HISTORY_NAME='{}_directory_history'.format(os.path.basename(cmds.file(query=True, sceneName=True)) or "untitled")
+DIRECTORY_HISTORY_ROOT_DIR = '{}/{}/prefs'.format(mm.eval('getenv "MAYA_APP_DIR";'), cmds.about(version=True))
+DIRECTORY_HISTORY_EXT= 'pck'
 
 #Colors
 LIGHT_GREEN=(.2,.4,.2)
@@ -55,6 +60,12 @@ def exportFBX(layer):
     #        baking_tools_core.auto_unwrap(selected_items)
     #    baking_tools_core.soft_texture_borders(selected_items)
     paths_dict = save_paths_to_file()
+    print(paths_dict)
+    if all(value == '' for value in paths_dict.values()):
+        cmds.warning(("Please browse and select a folder to export the layer to"))
+        return
+    if paths_dict[layer]=='':
+        paths_dict[layer]=next(iter(paths_dict.values()))
     path_to_export = paths_dict[layer]
     export_name_with_path='{}/{}'.format(path_to_export,layer)
     if not cmds.pluginInfo('fbxmaya', query=True, loaded=True):
@@ -80,6 +91,8 @@ def read_directories_from_browse_buttons():
     """
     paths_dict={}
     for layer, button in BROWSE_BUTTON_NAME_DICT.items():
+        if button == 'defaultLayer_browse_button':
+            continue
         path=cmds.button(button, query=True, annotation=True)
         paths_dict[layer]=path
     return paths_dict
@@ -90,8 +103,8 @@ def write_directory_to_file(directory_history_name, directory_dict):
     file_path=os.path.join(DIRECTORY_HISTORY_ROOT_DIR, file_name)
 
     try:
-        with open(file_path,'w') as f:
-            json.dump(directory_dict, f, indent=4)
+        with open(file_path,'wb') as f:
+            pickle.dump(directory_dict,f)
     except IOError:
         cmds.warning((f"Failed to write directory history to {file_path}"))
 
@@ -108,8 +121,8 @@ def read_directory_from_file():
         return directory_dict
     
     try:
-        with open(file_path, 'r') as f:
-            directory_dict = json.load(f)
+        with open(file_path, 'rb') as f:
+            directory_dict = pickle.load(f)
     except IOError:
         cmds.warning(f"Failed to read directory history from {file_path}")
 
@@ -136,10 +149,32 @@ def toggle_display_type(layer, checkbox):
     elif new_state == 2:
         cmds.checkBox(checkbox, edit=True, label="R", value=True)
 
-def set_layer_color(layer, color_field):
-    color = cmds.colorSliderGrp(color_field, query=True, rgbValue=True)
+def set_layer_color(layer):
+    color = cmds.colorEditor()
+    if cmds.colorEditor(query=True, result=True):
+        rgb = cmds.colorEditor(query=True, rgb=True)
+        cmds.setAttr(layer + ".overrideColorRGB", rgb[0], rgb[1], rgb[2])
+        cmds.setAttr(layer + ".overrideRGBColors", 1)
+        update_display_layer_ui()
+
+def set_layer_color(layer, color_field=None):
+    if color_field:
+        color = cmds.colorSliderGrp(color_field, query=True, rgbValue=True)
+    else:
+        color = cmds.colorEditor()
+        if not cmds.colorEditor(query=True, result=True):
+            return  # User cancelled the color editor
+        color = cmds.colorEditor(query=True, rgb=True)
+
     cmds.setAttr(layer + ".overrideColorRGB", color[0], color[1], color[2])
     cmds.setAttr(layer + ".overrideRGBColors", 1)
+    update_display_layer_ui()
+
+def get_layer_color(layer):
+    if cmds.attributeQuery("overrideColorRGB", node=layer, exists=True):
+        return cmds.getAttr(layer + ".overrideColorRGB")[0]
+    else:
+        return [0, 0, 0]  # Default color if not set
 
 def add_layer(*args):
     selected_objects = cmds.ls(selection=True)
@@ -157,17 +192,28 @@ def rename_layer(layer, text_field):
         update_display_layer_ui()
 
 def update_display_layer_ui():
-    if cmds.workspaceControl("displayLayerWorkspaceControl", exists=True):
-        cmds.deleteUI("displayLayerWorkspaceControl", control=True)
+    if cmds.workspaceControl(DISPLAY_LAYER_WORKSPACE_CONTROL_NAME, exists=True):
+        cmds.deleteUI(DISPLAY_LAYER_WORKSPACE_CONTROL_NAME, control=True)
     create_display_layer_ui()
 
+def delete_layer(layer):
+    if layer and layer != "defaultLayer":
+        cmds.delete(layer)
+        update_display_layer_ui()
+
+def add_objects_to_layer(layer):
+    selected_objects = cmds.ls(selection=True)
+    if selected_objects:
+        cmds.editDisplayLayerMembers(layer, selected_objects)
+        update_display_layer_ui()
+
 def create_display_layer_ui():
-    if cmds.workspaceControl("displayLayerWorkspaceControl", exists=True):
-        cmds.deleteUI("displayLayerWorkspaceControl", control=True)
+    if cmds.workspaceControl(DISPLAY_LAYER_WORKSPACE_CONTROL_NAME, exists=True):
+        cmds.deleteUI(DISPLAY_LAYER_WORKSPACE_CONTROL_NAME, control=True)
 
     BROWSE_BUTTON_NAME_DICT.clear()
 
-    workspace_ctrl = cmds.workspaceControl("displayLayerWorkspaceControl", label="Display Layer Editor")
+    workspace_ctrl = cmds.workspaceControl(DISPLAY_LAYER_WORKSPACE_CONTROL_NAME, label="Display Layer Editor")
     cmds.columnLayout(adjustableColumn=True)
 
     cmds.text(label="Display Layers", align='left', height=20)
@@ -175,12 +221,10 @@ def create_display_layer_ui():
     layer_list = cmds.ls(type="displayLayer")
 
     for layer in layer_list:
-        BROWSE_BUTTON_NAME_DICT[layer]='{}_browse_button'.format(layer)
+        BROWSE_BUTTON_NAME_DICT[layer] = '{}_browse_button'.format(layer)
 
     paths_dict = read_directory_from_file()
 
-    print(paths_dict)
-    
     for layer in layer_list:
         if layer == "defaultLayer":
             continue  # Skip the default layer
@@ -188,11 +232,9 @@ def create_display_layer_ui():
         layer_row_layout = cmds.rowLayout(numberOfColumns=9, adjustableColumn=True, columnAlign=(1, 'left'))
         
         # Layer Name
-        text_field = cmds.textField(text=layer, width=100)
-        
-        # Rename Button
-        cmds.button(label="Rename", command=lambda *args, l=layer, tf=text_field: rename_layer(l, tf))
-        
+        text_field = cmds.textField(width=100)
+        cmds.textField(text_field, edit=True, text=layer, changeCommand=lambda *args, l=layer, tf=text_field: rename_layer(l, tf))
+
         # Visibility Checkbox
         visibility = cmds.getAttr(layer + ".visibility")
         cmds.checkBox(label="V", value=visibility, width=30, 
@@ -203,6 +245,11 @@ def create_display_layer_ui():
         display_type = cmds.getAttr(layer + ".displayType")
         checkbox_label = " " if display_type == 0 else "T" if display_type == 1 else "R" if display_type == 2 else "T"
         template_checkbox = cmds.checkBox(label=checkbox_label, value=(display_type != 0), width=30)
+        
+        # Color Box
+        layer_color = get_layer_color(layer)
+        color_box = cmds.iconTextButton(style='textOnly', label="", width=30, bgc=layer_color,
+                                        command=lambda *args, l=layer: set_layer_color(l))  
         
         # Correctly reference the checkbox
         cmds.checkBox(template_checkbox, edit=True, 
@@ -224,24 +271,20 @@ def create_display_layer_ui():
             color = [0, 0, 0]  # Default color if not set
 
         color_field = cmds.colorSliderGrp(label="Color", rgb=(color[0], color[1], color[2]), width=100)
-        cmds.colorSliderGrp(color_field, edit=True, changeCommand=lambda *args, l=layer, cf=color_field: set_layer_color(l, cf))
-        
+        cmds.colorSliderGrp(color_field, edit=True, changeCommand=lambda *args, l=layer, cf=color_field: set_layer_color(l, cf))    
+
         # Browse button
         cmds.button(BROWSE_BUTTON_NAME_DICT[layer], label="...", width=50, command=lambda *args, b=BROWSE_BUTTON_NAME_DICT[layer]: browse(b), annotation=paths_dict[layer])
         
         # Export Button
-        cmds.button(label="Export", width=100, c = lambda *args, l=layer:exportFBX(l))
+        cmds.button(label="Export", width=100, command=lambda *args, l=layer: exportFBX(l))
+        
+        # Add a right-click context menu to the text field
+        cmds.popupMenu(parent=text_field)
+        cmds.menuItem(label="Delete Layer", command=lambda *args, l=layer: delete_layer(l))
+        cmds.menuItem(label="Add Selected Objects", command=lambda *args, l=layer: add_objects_to_layer(l))
         
         cmds.setParent('..')  # End rowLayout
     
     # Add Layer Button
     cmds.button(label="Add Layer", command=add_layer)
-
-    # Close Button
-    cmds.button(label="Close", command=("cmds.deleteUI('displayLayerWorkspaceControl', control=True)"))
-    cmds.setParent('..')
-
-#create_display_layer_ui()
-
-# export_tool_window().show_ui()
-# create_display_layer_ui().show_ui()
